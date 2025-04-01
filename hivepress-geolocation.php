8b069ebdd38082d431e403cf42b631e17aaf6d6a
Ondo <ondo@qtakehd.com>
2025-04-01 20:09:50 +0200

    Update hivepress-geolocation.php

--- hivepress-geolocation.php
+++ hivepress-geolocation.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: HivePress Geolocation
- * Description: Allow users to search listings by location.
+ * Description: Allow users to search listings by location. Replaced Mapbox with Maplibre as map provider.
  * Version: 1.3.7
- * Author: HivePress
- * Author URI: https://hivepress.io/
+ * Author: HivePress & xbelmondo
+ * Author URI: https://github.com/xbelmondo/hivepress-geolocation
  * Text Domain: hivepress-geolocation
  * Domain Path: /languages/
  *
